bcf7ce56c7007590a470b196d2c8ad5b141c6fd0
vonaxl <vonaxlcorso@gmail.com>
2019-11-11 17:07:03 +0100

    puuushh

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -1,11 +1,54 @@
 $( document ).ready(function() {
 
-    $("#send").click(function(){
+    setTimeout(function(){ 
+        var msgCopy = $("#template .msgpc").clone();
+        msgCopy.find(".txt").text("Ciao")
+        $("#chatbox").append(msgCopy)
+     }, 3000);
+    setTimeout(function(){ 
+        var msgCopy = $("#template .msgpc").clone();
+        msgCopy.find(".txt").text("Come va?")
+        $("#chatbox").append(msgCopy)
+     }, 6000);
+    setTimeout(function(){ 
+        var msgCopy = $("#template .msgpc").clone();
+        msgCopy.find(".txt").text("ti va di uscire?")
+        $("#chatbox").append(msgCopy)
+     }, 10000);
+    setTimeout(function(){ 
+        var msgCopy = $("#template .msgpc").clone();
+        msgCopy.find(".txt").text("Conosci per caso Michele de Rosa?")
+        $("#chatbox").append(msgCopy)
+     }, 15000);
+
+
+     function sendmsgs(){
         // get message on click from the input
         var message=$("#msg").val();
         console.log(message);
+
+        // copy the message template
+        var msgCopy = $("#template .msgsent").clone();
+        msgCopy.find(".txt").text(message)
+
+        // send the message with it's template
+        $("#chatbox").append(msgCopy)
+
         // clear the input 
         $("#msg").val("");
-    });
+    };
+
+    $("#send").click(sendmsgs);
+
+    $(window).on({
+        keyup: function (k) {
+            // console.log(k); //check which keycode 
+            
+            if (k.keyCode == "13") {
+                sendmsgs();
+            }
+            
+        }
+    })
     
 });
