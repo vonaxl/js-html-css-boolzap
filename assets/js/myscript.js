1cc9b331201d7cf0c50a010ca0aa0e689ac27b06
vonaxl <vonaxlcorso@gmail.com>
2019-11-11 16:08:30 +0100

    message start

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -1,4 +1,11 @@
 $( document ).ready(function() {
 
+    $("#send").click(function(){
+        // get message on click from the input
+        var message=$("#msg").val();
+        console.log(message);
+        // clear the input 
+        $("#msg").val("");
+    });
     
 });
