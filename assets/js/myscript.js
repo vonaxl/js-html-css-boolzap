c738c0ca5b41fb04ac1ffc1715f50d85f41964d9
vonaxl <vonaxlcorso@gmail.com>
2019-11-13 18:28:57 +0100

    puush finitoo

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -14,21 +14,15 @@ $( document ).ready(function() {
         msgCopy.find(".time").text(timer)
         $("#chatbox").prepend(msgCopy)
      }, 6000);
-    setTimeout(function(){ 
-        timer();
-        var msgCopy = $("#template .msgpc").clone();
-        msgCopy.find(".txt").text("ti va di uscire?")
-        msgCopy.find(".time").text(timer)
-        $("#chatbox").prepend(msgCopy)
-     }, 10000);
-    setTimeout(function(){ 
-        timer();
-        var msgCopy = $("#template .msgpc").clone();
-        msgCopy.find(".txt").text("Conosci per caso Michele de Rosa?")
-        msgCopy.find(".time").text(timer)
-        $("#chatbox").prepend(msgCopy)
-     }, 15000);
 
+    //  function that reply okays for each word sent
+     function ok(){ 
+        timer();
+        var msgCopy = $("#template .msgpc").clone();
+        msgCopy.find(".txt").text("ok");
+        msgCopy.find(".time").text(timer);
+        $("#chatbox").prepend(msgCopy);
+     }
 
      function sendmsgs(){
         timer()
@@ -45,6 +39,9 @@ $( document ).ready(function() {
         timer();
         // clear the input 
         $("#msg").val("");
+
+        setTimeout(ok,1000);
+    
     };
     // HOURS AND MINUTES FUNCTIONS
     function addZero(i) {
@@ -72,6 +69,61 @@ $( document ).ready(function() {
             }
             
         }
-    })
-    
+    });
+
+    $("#searchbar").keyup(function () {
+        var kPress = $('#searchbar').val();
+        var gPress = kPress.toLocaleLowerCase();
+        // name on each h6
+        // var nameH6="";
+        
+        if($(this).val() == ""){
+            $('.contact').removeClass('importantHide')
+        }
+         else {
+            $('.contact').addClass('importantHide');
+
+
+            // cerca per ogni .contact il suo nome
+            $(".contact").each(function(){
+                if ($(this).find('.box>h6').text().toLocaleLowerCase().includes(gPress)) {
+                    console.log(gPress);
+                    $(this).removeClass('importantHide')
+                }
+
+                // var name = [];
+                // var x = $(this).find('.box>h6').text()
+                // name.push(x)
+                // console.log(name);     
+                // // controlla quella h6 ha la lettera inserita nella searchbar
+                
+                // var z="",g="";
+                // for (var y = 0; y<name[0].length ; y++){
+                //     z=name[0][y];
+                //     g+=name[0][y];
+                //     l=z.toLocaleLowerCase();
+                //     if(gPress==l){
+                //         nameH6=name[0];
+                //     }
+                // }
+                
+                // console.log(g);
+                // console.log(nameH6);
+                
+                // // console.log(y);
+                
+            });
+
+
+
+
+
+            // $(".contact").each(function(){
+            //     if ($(this).find('.box>h6').val(kPress)) {
+            //         $("h6").parents(".content").removeClass('importantHide')
+            //     }
+            // });
+        }
+    });
+
 });
